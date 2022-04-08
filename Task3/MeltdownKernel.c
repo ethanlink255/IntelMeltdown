ab20096d3147c6bf8498af2cc758c5646964da8d
Ethan Link <linkeh@mail.uc.edu>
2022-04-08 14:09:05 -0400

    Task3 Complete

--- Task3/MeltdownKernel.c
+++ Task3/MeltdownKernel.c
@@ -7,7 +7,7 @@
 #include <linux/seq_file.h>
 #include <linux/uaccess.h>
 
-static char secret[8] = {’S’, ’E’, ’E’, ’D’, ’L’, ’a’, ’b’, ’s’};
+static char secret[8] = {'S', 'E', 'E', 'D', 'L', 'a', 'b', 's'};
 static struct proc_dir_entry *secret_entry;
 static char* secret_buffer;
 
@@ -22,20 +22,18 @@ static ssize_t read_proc(struct file *filp, char *buffer, size_t length, loff_t 
     memcpy(secret_buffer, &secret, 8); 
     return 8;
 }
-static const struct file_operations test_proc_fops = {
-    .owner = THIS_MODULE,
-    .open = test_proc_open,
-    .read = read_proc,
-    .llseek = seq_lseek,
-    .release = single_release,
+static const struct proc_ops test_proc_fops = {
+    .proc_open = test_proc_open,
+    .proc_read = read_proc,
+    .proc_lseek = seq_lseek,
+    .proc_release = single_release,
 };
 static __init int test_proc_init(void){
     // write message in kernel message buffer
-    printk("secret data address:%p\n", &secret); ¡
+    printk("secret data address:%p\n", &secret);
     secret_buffer = (char*)vmalloc(8);
     // create data entry in /proc
-    secret_entry = proc_create_data("secret_data",
-    0444, NULL, &test_proc_fops, NULL); ¬
+    secret_entry = proc_create_data("secret_data", 0444, NULL, &test_proc_fops, NULL);
     if (secret_entry) return 0;
     return -ENOMEM;
 }
